c84d5fc09556c5136c98e75c9a89ead2108e3c4b
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2024-01-19 01:45:02 +0100

    Add queries to Products

--- front/src/components/Product.tsx
+++ front/src/components/Product.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useQuery } from 'react-query'
+
+export type ProductType = {
+    product_id: number
+    product_name: string
+    unit_price: number
+}
 
 export const Product = () => {
+    const params = useParams()
+
+    const { data, isLoading } = useQuery({
+        queryKey: ['product'],
+        queryFn: async (): Promise<ProductType> => {
+            const res = await fetch(`http://localhost:5000/products/${params.id}`)
+            return res.json()
+        }
+    })
+
+    console.log({ data, isLoading })
+
+    if (isLoading) return <div>Loading...</div>
+
     return (
-        <div>Product</div>
+        <div className="border-2 rounded-xl flex flex-col p-10 w-max gap-2">
+            <h4 className='text-2xl '>Product</h4>
+            <div className='flex gap-2'>
+                <h6>ID</h6>
+                <p>{data?.product_id}</p>
+            </div>
+            <div className='flex gap-2'>
+                <h6>Name</h6>
+                <p>{data?.product_name}</p>
+            </div>
+            <div className='flex gap-2'>
+                <h6>Price</h6>
+                <p>{data?.unit_price}</p>
+            </div>
+        </div>
     )
 }
--- front/src/components/Products.tsx
+++ front/src/components/Products.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import { useQuery } from 'react-query'
+
+import { ProductType } from './Product'
 
 export const Products = () => {
+    const { data, isLoading } = useQuery({
+        queryKey: ['products'],
+        queryFn: async (): Promise<ProductType[]> => {
+            const res = await fetch('http://localhost:5000/products')
+            return res.json()
+        }
+    })
+
+    console.log({ data, isLoading })
+
+    if (isLoading) return <div>Loading...</div>
+
     return (
-        <div className='bg-green-400 w-full h-screen'>Products</div>
+        <div className="container mx-auto p-4">
+            <table className="min-w-full text-white border border-gray-300">
+                <thead>
+                    <tr>
+                        <th className="py-2 px-4 border-b">Nombre</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data?.map((product) => (
+                        <tr key={product.product_id}>
+                            <td className="py-2 px-4 border-b">
+                                <Link
+                                    to={`/productos/${product.product_id}`}
+                                    className="text-blue-500 hover:underline"
+                                >
+                                    {product.product_name}
+                                </Link>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+
     )
 }
