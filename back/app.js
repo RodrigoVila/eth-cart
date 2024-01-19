9bbc6d7db61107e48ff068280de3c0d98ee50162
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2024-01-19 01:43:29 +0100

    Add cors to server

--- back/app.js
+++ back/app.js
@@ -1,9 +1,11 @@
+const cors = require('cors')
 const express = require('express')
 const db = require('./dbConnection')
-
 const PORT = 5000
 
 const app = express()
+
+app.use(cors())
 app.listen(PORT, () => console.log("Server listening at: ", 5000))
 
 app.get('/products', async (_, res) => {
@@ -19,7 +21,7 @@ app.get('/products', async (_, res) => {
 app.get('/products/:id', async (req, res) => {
     try {
         const [results] = await db.q(`select * from Products where product_id=${req.params.id}`, [])
-        res.json({ status: res.status, data: results })
+        res.json(results)
     } catch (error) {
         console.log("Error getting products: ", error)
         res.send({ error })
