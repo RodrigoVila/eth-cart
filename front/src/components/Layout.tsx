6cd6b2accb9908eeb2a6ad0dded0142c4d3251b6
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2024-01-19 01:45:44 +0100

    Update layout styles

--- front/src/components/Layout.tsx
+++ front/src/components/Layout.tsx
@@ -2,12 +2,12 @@ import { Link, Outlet } from "react-router-dom";
 
 export const Layout = () => {
     return (
-        <div className="w-full h-screen">
+        <div className="w-full h-full min-w-screen">
             <nav className="w-full h-12 bg-slate-800 flex items-center justify-center text-white gap-3 font-mono font-bold m-0">
                 <Link to="/cart" className="hover:text-lg duration-150">Cesta</Link>
                 <Link to="/products" className="hover:text-lg duration-150">Productos</Link>
             </nav>
-            <main className="bg-slate-900 w-full h-screen">
+            <main className="bg-slate-900 w-full h-full min-h-screen text-white flex items-center justify-center">
                 <Outlet />
             </main>
         </div>
