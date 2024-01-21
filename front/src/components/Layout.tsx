2282a1c207acf23c4b96d8e809e0f22d39e929ee
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2024-01-21 20:44:10 +0100

    Fix layout

--- front/src/components/Layout.tsx
+++ front/src/components/Layout.tsx
@@ -1,13 +1,20 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 export const Layout = () => {
+    const { pathname } = useLocation()
+
+    const isProductsPath = pathname.includes("products")
+    const isCartPath = pathname.includes("cart")
+
+    const bgColor = isProductsPath ? "bg-slate-900" : "bg-slate-950"
+
     return (
         <div className="w-full h-full min-w-screen">
-            <nav className="w-full h-12 bg-slate-800 flex items-center justify-center text-white gap-3 font-mono font-bold m-0">
-                <Link to="/cart" className="hover:text-lg duration-150">Cesta</Link>
-                <Link to="/products" className="hover:text-lg duration-150">Productos</Link>
+            <nav className="w-full px-6 h-12 bg-slate-800 flex items-center justify-end text-white gap-6 font-bold m-0">
+                <Link to="/cart" className={`hover:text-xl duration-300 ${isCartPath ? "text-xl" : ""}`}>Cart</Link>
+                <Link to="/products" className={`hover:text-xl duration-300 ${isProductsPath ? "text-xl" : ""}`}>Products</Link>
             </nav>
-            <main className="bg-slate-900 w-full h-full min-h-screen text-white flex items-center justify-center">
+            <main className={`w-full h-full min-h-screen text-white flex items-center justify-center p-10 ${bgColor}`}>
                 <Outlet />
             </main>
         </div>
