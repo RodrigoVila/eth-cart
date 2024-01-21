11001d86e7a1466fe3abbcdac5762c7a7a3874e6
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2024-01-21 20:43:56 +0100

    Add cart logic

--- front/src/components/Cart.tsx
+++ front/src/components/Cart.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { useContext } from 'react'
+import { Context } from '../context'
+import { ProductsTable } from './ProductTableList'
+import { Button } from './Button'
 
 export const Cart = () => {
-    return (
-        <div className='bg-orange-300 w-full h-screen'>Cart</div>
-    )
+    const { state } = useContext(Context)
+    const total = state.cart.reduce((acc, { item_total }) => acc + item_total, 0)
+    const isCartEmpty = state.cart.length === 0
+
+    return isCartEmpty ?
+        <div className='text-2xl'>Cart is empty</div>
+        : (<div className='flex flex-col items-center gap-8'>
+            <ProductsTable products={state.cart} isCartTable />
+            <h3 className='text-2xl font-bold'>{`Total $ ${total}`}</h3>
+            <Button className='px-10 text-2xl' onClick={() => { }} >
+                Pay
+            </Button>
+        </div>
+        )
 }
